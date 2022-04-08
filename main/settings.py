8a5a3291dc35f14c3d800bd7013d473dcf5491d2
warlock <h.kwiatkowski@hotmail.com>
2022-04-08 18:06:33 +0200

    heroku config fix

--- main/settings.py
+++ main/settings.py
@@ -84,17 +84,18 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'todolist',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
 
+# Heroku database config
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
