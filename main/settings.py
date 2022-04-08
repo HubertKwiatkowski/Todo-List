f6b70907f08d713ba19b8ee4761f76f2b97c55ea
warlock <h.kwiatkowski@hotmail.com>
2022-04-08 18:04:54 +0200

    heroku config fix

--- main/settings.py
+++ main/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 #     }
 # }
 
-DATABASE['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
